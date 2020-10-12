698944b0606d7c8704bc7dcb6e3b9965a2326792
Donavon Buchanan <5569820+donavoncade@users.noreply.github.com>
2020-10-12 06:13:22 -0400

    Update version

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// v1.0.2-beta
+
 import PackageDescription
 
 let package = Package(
