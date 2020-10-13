c3e0456d569896ae190e71fe17e1e6665533b663
Donavon Buchanan <5569820+donavoncade@users.noreply.github.com>
2020-10-13 01:13:31 -0400

    Testing fix for filled lazy view frames

--- Sources/SimpleGeometry/Modifiers/FilledFrameModifier.swift
+++ Sources/SimpleGeometry/Modifiers/FilledFrameModifier.swift
@@ -18,12 +18,12 @@ struct FilledFrameModifier: ViewModifier {
         case .fill:
             content
                 .frame(
-                    minWidth: 0,
-                    idealWidth: 0,
-                    maxWidth: behavior == .fill ? .infinity : rect.width,
-                    minHeight: 0,
-                    idealHeight: 0,
-                    maxHeight: behavior == .fill ? .infinity : rect.height,
+                    minWidth: .none,
+                    idealWidth: .none,
+                    maxWidth: behavior == .fill ? .infinity : .none,
+                    minHeight: .none,
+                    idealHeight: .none,
+                    maxHeight: behavior == .fill ? .infinity : .none,
                     alignment: alignment ?? .center
                 )
         default:
